53ecc4551638b2f002804d2a0e6348898771490b
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2023-12-29 15:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const openBtn = document.querySelector("#openModal")
+const closeBtn = document.querySelector(".close-modal")
+const modal = document.querySelector(".modal")
+
+openBtn.addEventListener("click", ()=>{
+    modal.classList.remove("hide");
+    openBtn.classList.add("hide");
+})  
+
+const closeModal = ()=>{
+    modal.classList.add("hide");
+    openBtn.classList.remove("hide");
+}
+
+closeBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
